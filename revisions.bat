8232a2ef641f9b87d7d2bcc5127fe59d3bdcb48b
Osamu Koizumi <Osamu.Koizumi@arm.com>
2019-04-11 10:37:14 +0900

    Removed added files for deploy command.

--- revisions.bat
+++ revisions.bat
@@ -1,4 +1,10 @@
 REM Change to the correct revisions
+
+REM These files should be deleted to run the deploy command.
+del mbed-os\.gitignore
+del simple-mbed-cloud-client\.gitignore
+
+REM run the deploy command
 mbed deploy
 mbed ls
 pause
